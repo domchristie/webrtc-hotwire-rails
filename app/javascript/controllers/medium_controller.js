615930afaf31817fe7dcd6071f0f4d29ba6a2774
Dom Christie <christiedom@gmail.com>
2022-02-28 08:46:52 -0800

    Remove redundant code now that stimulus supports ad/remove callbacks

--- app/javascript/controllers/medium_controller.js
+++ app/javascript/controllers/medium_controller.js
@@ -5,14 +5,6 @@ export default class MediumController extends Controller {
     this.reRenderMediaElement()
   }
 
-  get roomController () {
-    return this.roomElement.room
-  }
-
-  get isRemote () {
-    return this.element.matches('[data-room-target="remoteMedium"]')
-  }
-
   // Fix potentially blank videos due to autoplay rules?
   reRenderMediaElement () {
     const mediaElement = this.mediaElementTarget
